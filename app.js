4637bd08beb348f5c633d1057d0eaaed671b28af
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-07-15 22:28:17 +0100

    setting up server

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express= require('express');
-const path = require('path')
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 7000
+const {config, engine} = require('express-edge');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 7000;
+const index = require('./routes/index')
+
+app.use(engine);
+app.set('views', __dirname + '/views');
 
 
 if (process.env.NODE_ENV = "Production" || process.env.NODE_ENV === "staging"){
     app.use(express.static('public'));
-    app.get('/', (req,res)=>{
-        res.sendFile(path.resolve(__dirname + '/reuse/set.json'))
- })
+    app.use(index);
 }
  
 
